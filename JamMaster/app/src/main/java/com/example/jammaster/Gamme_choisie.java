cc5f654f2e2415422291eef8d55b868fbe7409fc
KUBASIK <tomkubasik74200@gmail.com>
2019-12-11 00:10:52 +0100

    Affichage de la gamme harmonisée dans un tableau propre

--- JamMaster/app/src/main/java/com/example/jammaster/Gamme_choisie.java
+++ JamMaster/app/src/main/java/com/example/jammaster/Gamme_choisie.java
@@ -9,15 +9,24 @@ import android.widget.TextView;
 public class Gamme_choisie extends AppCompatActivity {
 
     private TextView noteTextView;
-    private TextView modeTextView;
-    private TextView gammeTextView;
+
+
+
 
     private int noteChoisie;
     private int modeChoisi;
 
     private String noteChoisieString;
     private String modeChoisiString;
-    private String gammeChoisieString;
+
+
+    private TextView textViewDegres1;
+    private TextView textViewDegres2;
+    private TextView textViewDegres3;
+    private TextView textViewDegres4;
+    private TextView textViewDegres5;
+    private TextView textViewDegres6;
+    private TextView textViewDegres7;
 
 
 
@@ -29,19 +38,41 @@ public class Gamme_choisie extends AppCompatActivity {
         Gamme_fonctions G = new Gamme_fonctions();
 
         noteTextView = (TextView) findViewById(R.id.text_view_note);
-        modeTextView = (TextView) findViewById(R.id.text_view_mode);
-        gammeTextView = (TextView) findViewById(R.id.text_view_gamme);
+
+
+
+
+        textViewDegres1 = (TextView) findViewById(R.id.degres1);
+        textViewDegres2 = (TextView) findViewById(R.id.degres2);
+        textViewDegres3 = (TextView) findViewById(R.id.degres3);
+        textViewDegres4 = (TextView) findViewById(R.id.degres4);
+        textViewDegres5 = (TextView) findViewById(R.id.degres5);
+        textViewDegres6 = (TextView) findViewById(R.id.degres6);
+        textViewDegres7 = (TextView) findViewById(R.id.degres7);
+
 
         noteChoisie = G.ChoisirNote();
         modeChoisi = G.ChoisirMode();
 
+        G.RemplirTableauGamme(noteChoisie, modeChoisi);
+
         noteChoisieString = G.ChoisirNoteTab(noteChoisie);
         modeChoisiString = G.ChoisirModeString(modeChoisi);
-        gammeChoisieString = G.GammeChoisie(noteChoisie, modeChoisi);
 
-        noteTextView.setText(noteChoisieString);
-        modeTextView.setText(modeChoisiString);
-        gammeTextView.setText(gammeChoisieString);
+
+        noteTextView.setText(noteChoisieString + " " + modeChoisiString) ;
+
+
+
+
+        textViewDegres1.setText(G.RemplirCase(0));
+        textViewDegres2.setText(G.RemplirCase(1));
+        textViewDegres3.setText(G.RemplirCase(2));
+        textViewDegres4.setText(G.RemplirCase(3));
+        textViewDegres5.setText(G.RemplirCase(4));
+        textViewDegres6.setText(G.RemplirCase(5));
+        textViewDegres7.setText(G.RemplirCase(6));
+
 
 
     }
--- JamMaster/app/src/main/java/com/example/jammaster/Gamme_fonctions.java
+++ JamMaster/app/src/main/java/com/example/jammaster/Gamme_fonctions.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Gamme_fonctions {
     Random r = new Random();
     String tabNotes[] = new String[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#"};
-    int tabDegresNotes[] = new int[] {0, 0, 0, 0, 0, 0, 0};
-    int tabDegresHarmo[] = new int[] {0, 0, 0, 0, 0, 0, 0};
+    int tabDegresNotes[] = new int[]{0, 0, 0, 0, 0, 0, 0};
+    int tabDegresHarmo[] = new int[]{0, 0, 0, 0, 0, 0, 0};
 
 
     int ChoisirNote() {
@@ -20,13 +20,28 @@ public class Gamme_fonctions {
         return i;
     }
 
-    String ChoisirModeString(int randNumMode){
+    String ChoisirModeString(int randNumMode) {
         String mode;
-        if (randNumMode == 0){
+        if (randNumMode == 0) {
             mode = "Majeur";
-        }
-        else if (randNumMode == 1) {
+        } else if (randNumMode == 1) {
             mode = "Mineur";
+        } else {
+            mode = "error";
+        }
+        return mode;
+    }
+
+    String ChoisirModeStringPourTableau(int numDegres) {
+        String mode;
+        if (numDegres == 0) {
+            mode = "";
+        }
+        else if (numDegres == 1) {
+            mode = "m";
+        }
+        else if (numDegres == 2) {
+            mode = "dim°";
         }
         else {
             mode = "error";
@@ -34,47 +49,45 @@ public class Gamme_fonctions {
         return mode;
     }
 
-    String ChoisirNoteTab(int randNumNote){
+    String ChoisirNoteTab(int randNumNote) {
         String s = tabNotes[randNumNote];
         return s;
     }
 
-    String GammeChoisie(int numNote, int numMode){
+    String GammeChoisie(int numNote, int numMode) {
         String s = "";
-        if (numMode == 0){
-            s = ChoisirNoteTab(numNote) + " " + ChoisirNoteTab(numNote+2) + "m " + ChoisirNoteTab(numNote+4) + "m " +
-                    ChoisirNoteTab(numNote+5) + " " + ChoisirNoteTab(numNote+7) + " " +
-                    ChoisirNoteTab(numNote+9) + "m " + ChoisirNoteTab(numNote+11) + "dim";
+        if (numMode == 0) {
+            s = ChoisirNoteTab(numNote) + " " + ChoisirNoteTab(numNote + 2) + "m " + ChoisirNoteTab(numNote + 4) + "m " +
+                    ChoisirNoteTab(numNote + 5) + " " + ChoisirNoteTab(numNote + 7) + " " +
+                    ChoisirNoteTab(numNote + 9) + "m " + ChoisirNoteTab(numNote + 11) + "dim";
+        } else if (numMode == 1) {
+            s = ChoisirNoteTab(numNote) + "m " + ChoisirNoteTab(numNote + 2) + "dim " + ChoisirNoteTab(numNote + 3) + " " +
+                    ChoisirNoteTab(numNote + 5) + "m " + ChoisirNoteTab(numNote + 7) + "m " +
+                    ChoisirNoteTab(numNote + 8) + " " + ChoisirNoteTab(numNote + 10) + "";
+        } else {
+            s = "error";
         }
-         else if (numMode == 1){
-            s = ChoisirNoteTab(numNote) + "m " + ChoisirNoteTab(numNote+2) + "dim " + ChoisirNoteTab(numNote+3) + " " +
-                    ChoisirNoteTab(numNote+5) + "m " + ChoisirNoteTab(numNote+7) + "m " +
-                    ChoisirNoteTab(numNote+8) + " " + ChoisirNoteTab(numNote+10) + "";
-        }
-         else{
-             s = "error";
-        }
-         return s;
+        return s;
     }
 
-    void RemplirTableauGamme(int numNote, int numMode){
-        if (numMode == 0){
-        tabDegresNotes[0] = numNote;
-        tabDegresHarmo[0] = 0;
-        tabDegresNotes[1] = numNote + 2;
-        tabDegresHarmo[1] = 1;
-        tabDegresNotes[2] = numNote + 4;
-        tabDegresHarmo[2] = 1;
-        tabDegresNotes[3] = numNote + 5;
-        tabDegresHarmo[3] = 0;
-        tabDegresNotes[4] = numNote + 7;
-        tabDegresHarmo[4] = 0;
-        tabDegresNotes[5] = numNote + 9;
-        tabDegresHarmo[5] = 1;
-        tabDegresNotes[6] = numNote + 11;
-        tabDegresHarmo[6] = 2;
-    }
-        if (numMode == 1){
+    void RemplirTableauGamme(int numNote, int numMode) {
+        if (numMode == 0) {
+            tabDegresNotes[0] = numNote;
+            tabDegresHarmo[0] = 0;
+            tabDegresNotes[1] = numNote + 2;
+            tabDegresHarmo[1] = 1;
+            tabDegresNotes[2] = numNote + 4;
+            tabDegresHarmo[2] = 1;
+            tabDegresNotes[3] = numNote + 5;
+            tabDegresHarmo[3] = 0;
+            tabDegresNotes[4] = numNote + 7;
+            tabDegresHarmo[4] = 0;
+            tabDegresNotes[5] = numNote + 9;
+            tabDegresHarmo[5] = 1;
+            tabDegresNotes[6] = numNote + 11;
+            tabDegresHarmo[6] = 2;
+        }
+        else if (numMode == 1) {
             tabDegresNotes[0] = numNote;
             tabDegresHarmo[0] = 1;
             tabDegresNotes[1] = numNote + 2;
@@ -82,7 +95,7 @@ public class Gamme_fonctions {
             tabDegresNotes[2] = numNote + 3;
             tabDegresHarmo[2] = 0;
             tabDegresNotes[3] = numNote + 5;
-            tabDegresHarmo[3] = 1
+            tabDegresHarmo[3] = 1;
             tabDegresNotes[4] = numNote + 7;
             tabDegresHarmo[4] = 1;
             tabDegresNotes[5] = numNote + 8;
@@ -90,4 +103,12 @@ public class Gamme_fonctions {
             tabDegresNotes[6] = numNote + 10;
             tabDegresHarmo[6] = 0;
         }
+    }
+
+    String RemplirCase (int numDegres){
+        String s;
+        s = ChoisirNoteTab(tabDegresNotes[numDegres]) + ChoisirModeStringPourTableau(tabDegresHarmo[numDegres]);
+        return s;
+    }
 }
+
